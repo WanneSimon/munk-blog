86b6b3d45167d10920368b70f2f2951410fa4678
wanne <wanneseville@foxmail.com>
2020-09-18 18:19:01 +0800

    添加 quill-editor，'书签'概念

---
+++ src/components/BookmarkFolder.vue
@@ -0,0 +1,29 @@
+<template>
+    <el-menu-item-group :title="bookmark.title" >
+      <el-menu-item index="3-1">选项1</el-menu-item>
+    </el-menu-item-group>
+</template>
+
+<script>
+  export default {
+    name: "BookmarkFolder",
+    props: { bookmarks:JSObject },
+    data() {
+      return {
+
+      }
+    },
+    created: function(){
+      this.bookmarks = [{
+        name: '文件夹1',
+        
+      }]
+    },
+    methods: {
+
+    }
+  }
+</script>
+
+<style>
+</style>
---
+++ src/views/bookmark.vue
@@ -0,0 +1,38 @@
+<template>
+   <div class="bookmark">
+      <el-container style="height: 500px; border: 1px solid #eee">
+        <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+          <el-menu >
+            <el-submenu>
+              
+            </el-submenu>
+          </el-menu>
+        </el-aside>
+
+      </el-container>
+   </div>
+</template>
+
+<script>
+  export default {
+    name: "bookmark",
+    // components: { SimEditor },
+    data() {
+      return {
+
+      }
+    },
+    created: function(){
+
+    },
+    methods: {
+
+    }
+
+  }
+</script>
+
+<style>
+
+
+</style>
