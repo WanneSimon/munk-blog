143a6e5e7ae286a15c0ca3147984ca4591d526fd
wanne <wanneseville@foxmail.com>
2020-11-01 12:11:16 +0800

    个人页

--- src/views/me.vue
+++ src/views/me.vue
@@ -9,20 +9,30 @@
       </el-carousel-item>
     </el-carousel>
 
-    <div class="wrapper">
+    <el-row class="wrapper">
+      <el-col class="blank_L" :span="4"></el-col>
+      <el-col :span="10">
+        <el-carousel class="little-wrapper" :interval="5000"
+              indicator-position="none" arrow="hover" @change="changeLittle">
+          <el-carousel-item class="little-item" v-for="item,key in images1" :key="key">
+            <!-- <h3>{{ item }}</h3> -->
+            <el-image :fit='fit' :src="item.url" >
+            </el-image>
+          </el-carousel-item>
+        </el-carousel>
+      </el-col>
 
-      <el-carousel :interval="5000" indicator-position="none" arrow="hover">
-        <el-carousel-item class="little-wrapper" v-for="item,key in images1" :key="key">
-          <!-- <h3>{{ item }}</h3> -->
-          <el-image class="little-item" :fit='fit' :src="item.url" >
-          </el-image>
-          <span class="little_text">{{item.text}}</span>
-        </el-carousel-item>
-      </el-carousel>
+      <el-col class="blank_L" :span="1"></el-col>
+      <el-col :span="8" class="little_text">
+        <span>{{rightText}}</span>
+      </el-col>
 
-      <!-- <div style="position:absolute; width:100%; height:10rem;"></div> -->
-    </div>
+      <el-col class="wrapper_bottom" :span="24">
+        进来看看？
+      </el-col>
+    </el-row>
 
+    <div class="me_bottom"> buttom </div>
   </div>
 </template>
 
@@ -43,6 +53,7 @@
         // imageFits: ['fill', 'contain', 'cover', 'none', 'scale-down'],
         fit: 'cover',
 
+        rightText: "这个人很懒，什么都没写！",
         images1: [ ],
       }
     },
@@ -58,53 +69,62 @@
         // 设置图片
         const baseDir = "/coco/static/images/me/"
         this.images1.push({url: baseDir + "1-1.png", text: '芭芭拉的密汁饮料？芭芭拉的黑暗饮料？'})
-        // this.images1.push({url:baseDir + "1-2.png", text: "可莉，淦！干！肝！"})
+        this.images1.push({url:baseDir + "1-2.png", text: "可莉，淦！干！肝！"})
       },
 
+      changeLittle: function(index){
+        this.rightText = this.images1[index].text
+      },
 
     }
   }
 </script>
 
 <style>
+  .me_container{
+    position: relative;
+  }
+
   .bg-images{
-    position: absolute;
-    width: 90%;
-    height: 60%;
-    left: 5%;
-    top: 1%;
-    z-index: -1;
+    width: 100%;
+    height: 30rem;
   }
   .el-carousel__container{
     height: 100% !important;
     width: 100% !important;
   }
-  .wrapper{
-    position: absolute;
-    bottom: 1%;
-    z-index: 0;
-    width: 92%;
-    left: 4%;
-    height: 70%;
-    background-color: rgba(1, 1, 1, 0.3);
+  .image-item{
+    width: 100%;
+    height: 100%;
   }
-
-  .little-wrapper{
-    margin-top: 10%;
-    height: 86%;
+  .wrapper{
+    width: 100%;
+    height: 30rem;
+    background-color: rgba(1, 1, 1, 0.3);
     position: relative;
+    top: 0rem;
+    margin-top: -8rem;
+  }
+  .little-wrapper{
+    height: 23rem;
   }
   .little-item{
-    width: 40%;
-    position: relative;
-    left: -15%;
-    /* position: ; */
   }
+
   .little_text{
-    position: absolute;
-    color: cornsilk;
-   /* top: 35%; */
-    left: 60%;
-    bottom: 8%;
+    margin-top: 10rem;
+    height: 10rem;
+  }
+  .wrapper_bottom{
+    margin-top: 1rem;
+    height: 7rem;
+    font-size: 1.5rem;
+    font-family: "arial black";
+  }
+
+  .me_bottom{
+    width: 100%;
+    height: 4rem;
+    background-color: rgba(1, 1, 1, 0.3);
   }
 </style>
